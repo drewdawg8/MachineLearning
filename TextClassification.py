ddedeb43c6144915a63dcc3cb8fd09749879e5fb
drew <dstewa17@masonlive.gmu.edu>
2018-03-09 23:32:27 -0500

    mm

--- TextClassification.py
+++ TextClassification.py
@@ -18,6 +18,7 @@ import itertools
 import os
 import pickle
 
+
 import numpy as np
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 path = './data/train/'
@@ -38,24 +39,32 @@ negative_encoded = process_for_model(negative_texts)
 #     while len(i)<350:
 #         i.append(-1)
 
-print(negative_encoded)
-print(positive_encoded)
+
+training_labels = []
+train_batches = []
+for i in range(len(positive_encoded)):
+    train_batches+=(positive_encoded[i])
+    training_labels.append([0])
+for i in range(len(negative_encoded)):
+    train_batches+=(negative_encoded[i])
+    training_labels.append([1])
+
+train_batches = array(train_batches)/1000
+training_labels = array(training_labels)
 #Scaler is used to get all of our input between 0 and 1
 scaler = MinMaxScaler(feature_range=(0,1))
-scaled_pos = scaler.fit_transform(array(positive_encoded,dtype=float).reshape(-1,1))
-scaled_neg = scaler.fit_transform(array(negative_encoded,dtype=float).reshape(-1,1))
+scaled_train = []
+print(training_labels.shape)
 
 model = Sequential([
-        Conv2D(64,(3,3),activation ='relu',input_shape =(224,224,3)),
-        Conv2D(32,(3,3),activation = 'relu'),
-        Flatten(),
-        Dense(2,activation = 'softmax')])
+        Dense(16,input_shape=(168,),activation='relu'),
+        Dense(2,activation='softmax')
+])
 
 #model = load_model('firstKeras.h5')
 model.compile(Adam(lr=.001),loss='categorical_crossentropy',metrics=['accuracy'])
 
 
-model.fit(train_batches,steps_per_epoch=15,epochs = 20, shuffle = True,verbose = 2)
-#predictions = model.predict_classes(scaled_tests,batch_size=10,verbose=0)
+model.fit(train_batches, training_labels, batch_size = 2,epochs = 20, shuffle = True,verbose = 2)#predictions = model.predict_classes(scaled_tests,batch_size=10,verbose=0)
 model.save('firstKeras.h5')
 K.clear_session()
